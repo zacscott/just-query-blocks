ec77429bdc734543f4058fca09f487ec48ff521a
Zac Scott <zac@zacscott.net>
2023-06-29 08:13:33 +0000

    register plugin block patterns

--- just-query-block.php
+++ just-query-block.php
@@ -15,3 +15,4 @@ define( 'JUST_QUERY_BLOCKS_PLUGIN_ABSURL', plugin_dir_url( __FILE__ )  );
 
 // Boot each of the plugin logic controllers.
 new \JustQueryBlocks\Controller\QueryBlocksController();
+new \JustQueryBlocks\Controller\PatternController();
